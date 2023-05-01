e00315b9f2f5704b13e89ec9e06e01bfdb9b52d5
ricardo-f <riclima1990@gmail.com>
2023-05-01 18:28:58 -0300

    Challenge solution

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# results-summary-component
+# Results summary component
+
+## Objective:
+Create a replica of the frontend mentor challenge called: [Results summary component](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV).
+
+## How:
+- Using HTML, CSS and JavaScript
+## Why?
+- Personal project.
+- Learn more about the used tools.
+- Share knowledge (that's why is public).
+
+### Extra docs and ref:
+- [FrontEnd Mentor](https://www.frontendmentor.io/)
+- [MDN HTML Reference](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [MDN CSS Reference](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [CSS Units](https://www.w3schools.com/cssref/css_units.php)
+- [What’s The Difference Between PX, EM, REM, %, VW, and VH?](https://elementor.com/help/whats-the-difference-between-px-em-rem-vw-and-vh/)
+- [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/fetch)
---
+++ assets/scripts/script.js
@@ -0,0 +1,30 @@
+fetch('assets/scripts/data.json')
+  .then(response => response.json())
+  .then(data => {
+    const list = document.getElementById('dynamic-list');
+    data.forEach(item => {
+      const li = document.createElement('li');
+      li.classList.add(item.category.toLowerCase());
+      const img = document.createElement('img');
+      img.src = item.icon;
+      img.alt = item.category;
+      const div1 = document.createElement('div');
+      div1.classList.add('icon');
+      div1.appendChild(img);
+      const div2 = document.createElement('div');
+      div2.classList.add('category');
+      div2.textContent = item.category;
+      const div3 = document.createElement('div');
+      div3.classList.add('score');
+      div3.textContent = item.score;
+      const div4 = document.createElement('div');
+      div4.classList.add('score-text');
+      div4.textContent = '/100';
+      li.appendChild(div1);
+      li.appendChild(div2);
+      li.appendChild(div3);
+      li.appendChild(div4);
+      list.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
